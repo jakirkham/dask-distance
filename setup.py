840e808ecb08f44ca634622d087d5456d4accf39
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-24 00:36:57 -0400

    Drop HISTORY document
    The template include a changelog, which we generally do not use as the
    changelog is recorded into the git tags and the GitHub releases.
    Including this in yet another document simply complicates the matter of
    doing releases and raises questions like how to automate generation of
    such a document.

--- setup.py
+++ setup.py
@@ -25,9 +25,6 @@ class PyTest(TestCommand):
 with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
 requirements = [
     # TODO: put package requirements here
 ]
@@ -46,7 +43,7 @@ setup(
     name="dask-distance",
     version=versioneer.get_version(),
     description="Distance computations with Dask (akin to scipy.spatial.distance)",
-    long_description=readme + "\n\n" + history,
+    long_description=readme,
     author="John Kirkham",
     author_email="kirkhamj@janelia.hhmi.org",
     url="https://github.com/jakirkham/dask-distance",
