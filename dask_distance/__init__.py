06a76e0f3f11a0c419406262ac92bff754cb6fa5
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-26 15:14:25 -0400

    Add hamming
    Implements a Dask array function for computing the Hamming distance
    between two 1-D bool arrays.

--- dask_distance/__init__.py
+++ dask_distance/__init__.py
@@ -41,3 +41,31 @@ def dice(u, v):
     )
 
     return result
+
+
+def hamming(u, v):
+    """
+    Finds the Hamming distance between two 1-D bool arrays.
+
+    .. math::
+
+       \\frac{ c_{TF} + c_{FT} }{ c_{TT} + c_{FT} + c_{TF} + c_{FF} }
+
+    where :math:`c_{XY} = \sum_{i} \delta_{u_{i} X} \delta_{v_{i} Y}`
+
+    Args:
+        u:           1-D bool array
+        v:           1-D bool array
+
+    Returns:
+        float:       Hamming distance
+    """
+
+    uv_mtx = _utils._bool_cmp_mtx_cnt(u, v)
+
+    result = (
+        (uv_mtx[1, 0] + uv_mtx[0, 1]) /
+        float(len(u))
+    )
+
+    return result
