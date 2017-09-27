b6ef372b33682de57ca3014ef9271407f0031be9
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-09-27 01:53:45 -0400

    Drop unfulfilled checks of `_bool_cmp_cnts`
    Some tests no longer make sense for `_bool_cmp_cnts` due to recent
    simplifications to it and movement of similar code into other functions
    like the wrapper.

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -39,23 +39,10 @@ def test__broadcast_uv():
 
 @pytest.mark.parametrize("et, u, v", [
     (ValueError, np.zeros((2,), dtype=bool), np.zeros((3,), dtype=bool)),
-    (ValueError, np.zeros((1, 2, 1,), dtype=bool), np.zeros((2,), dtype=bool)),
-    (ValueError, np.zeros((2,), dtype=bool), np.zeros((1, 2, 1,), dtype=bool)),
 ])
 def test__bool_cmp_cnts_err(et, u, v):
     with pytest.raises(et):
         dask_distance._utils._bool_cmp_cnts(u, v)
-
-
-def test__bool_cmp_cnts_1d():
-    u = np.array([0, 0, 0, 1, 1, 1, 1, 1, 1, 1], dtype=bool)
-    v = np.array([0, 1, 1, 0, 0, 0, 1, 1, 1, 1], dtype=bool)
-
-    uv_cmp_mtx = dask_distance._utils._bool_cmp_cnts(u, v)
-
-    uv_cmp_mtx_exp = np.array([[1, 2], [3, 4]], dtype=float)
-
-    assert (np.array(uv_cmp_mtx) == uv_cmp_mtx_exp).all()
 
 
 def test__bool_cmp_cnts_nd():
