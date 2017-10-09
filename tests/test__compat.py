ca8a9e74241719a74b0e6bbf915e58a3daedd290
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-10-08 21:26:07 -0400

    Test atleast_2d
    Provide some basic tests to exercise `atleast_2d`.

--- tests/test__compat.py
+++ tests/test__compat.py
@@ -32,3 +32,27 @@ def test_asarray(x):
         x = np.asarray(x)
 
     dau.assert_eq(d, x)
+
+
+@pytest.mark.parametrize("a", [
+    2,
+    np.array(2),
+    np.array([2, 3]),
+    np.array([[2], [3]]),
+    np.array([[2, 3]]),
+    np.array([[2, 3], [4, 5]]),
+    np.array([[[2, 3], [4, 5]], [[6, 9], [7, 1]]]),
+    [np.array(2), np.array([[2, 3]])],
+])
+def test_atleast_2d(a):
+    if not isinstance(a, list):
+        a = [a]
+
+    d = dask_distance._compat._atleast_2d(*a)
+
+    if not isinstance(d, list):
+        d = [d]
+
+    for d_i in d:
+        assert isinstance(d_i, da.Array)
+        assert d_i.ndim >= 2
