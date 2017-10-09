abfcee2507eb9b6ceb72aec7770da39bf1c82966
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-10-08 21:25:57 -0400

    Add atleast_2d
    Implements an internal implementation of `atleast_2d`. Exists for
    compatibility reasons and support. Will be dropped once Dask Array
    supports it.

--- dask_distance/_compat.py
+++ dask_distance/_compat.py
@@ -26,3 +26,33 @@ def _asarray(a):
         a = dask.array.from_array(a, a.shape)
 
     return a
+
+
+def _atleast_2d(*arys):
+    """
+    Provide at least 2-D views of the arrays.
+
+    Parameters
+    ----------
+    *arys : Dask Array
+            Arrays to make at least 2-D
+
+    Returns
+    -------
+    *res : Dask Array sequence
+    """
+
+    result = []
+    for a in arys:
+        a = _asarray(a)
+        if a.ndim == 0:
+            a = a[None, None]
+        elif a.ndim == 1:
+            a = a[None]
+
+        result.append(a)
+
+    if len(arys) == 1:
+        result = result[0]
+
+    return result
